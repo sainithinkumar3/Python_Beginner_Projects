4d50037ef82de190478c4e02b7a8be167e3bbb8e
sainithinkumar3 <sainithinkumar3@gmail.com>
2018-09-13 04:55:03 +0530

    Commit 2 for Dice_Rolling_Simulator.py

--- Dice_Rolling_Simulator.py
+++ Dice_Rolling_Simulator.py
@@ -1,13 +1,16 @@
 import random
 import itertools
-no_sides=int(input("Enter no.of sides:\t"))
-min,max=int(input("Enter min and max separated by comma as min_val,max_val"))
+#no_sides=int(input("Enter no.of sides:  "))
+xi=input("Enter min and max as #min max: ")
+min=int(xi[0])
+max=int(xi[2])
 def Roll_It():
 	for i in itertools.repeat(1):
-		print("You have "+random.randint(min,max+1)+"\n")
-		check=char(input("Do yow want to roll again(y/n)"))
+		print("You have "+str(random.randint(min,max)))
+		check=input("Do yow want to roll again(y/n) ")
 		if(check=='y' or check=='Y'):
 			continue
 		else:
 			break
+	print("\t\tThank_You\t\t");
 Roll_It()
