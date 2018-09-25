c76369e37524d77e59e432131e7085ffdf90e32b
nithin <nithin@localhost.localdomain>
2018-09-25 23:30:06 +0530

    Added Text_Based_Tic_Tac_Toe_Game.py (level-2)

--- Text_Based_Tic_Tac_Toe_Game.py
+++ Text_Based_Tic_Tac_Toe_Game.py
@@ -1,4 +1,5 @@
 import random
+import numpy
 win=str()
 def check(bb):
 	global win
@@ -39,23 +40,49 @@ def check(bb):
 				return False
 
 	return False
+def Think(bb,cc):
+	s=[0,3,6,0,1,2,0,2]
+	c=[1,1,1,3,3,3,4,2]
+	for i in range(8):
+		l={s[i]:bb[s[i]],int(s[i]+c[i]):bb[s[i]+c[i]],int(s[i]+2*c[i]):bb[s[i]+2*c[i]]}
+		if l.values().count(" ")==1:
+			if l.values().count("O")==2:
+				return l.keys()[l.values().index(" ")]
+	for i in range(8):
+                l={s[i]:bb[s[i]],int(s[i]+c[i]):bb[s[i]+c[i]],int(s[i]+2*c[i]):bb[s[i]+2*c[i]]}
+                if l.values().count(" ")==1:
+                        if l.values().count("X")==2:
+                                return l.keys()[l.values().index(" ")]
+	return random.choice(cc)
 def Tic_Tac_Toe():
 	print("Welcome to Tic Tack Toe. Player is X, Computer is O. This is a new game. Board numbers are as follows:")
 	for i in [1,4,7]:
 		print("\t"+str(i)+" "+"|"+" "+str(i+1)+" "+"|"+" "+str(i+2))
-	user_ip=list()
-	cmp_ip=list()
+	user_ip=int()
+	ip=int()
+	cmp_ip=int()
 	board=list()
-	choice=[1,2,3,4,5,6,7,8,9]
+	choice=[0,1,2,3,4,5,6,7,8]
 	for i in range(9):
 		board.append(" ")
 	iter=True
 	count=0
 	con_st=True
 	while(iter):
-		user_ip.append(input("Enter your desired location[1-9] : "))
-		choice.remove(user_ip[len(user_ip)-1])
-		board[user_ip[len(user_ip)-1]-1]="X"
+		c_ip=0
+		while True:
+			if c_ip==0:
+				ip=int(input("Enter your desired location[1-9] : "))
+			else:
+				ip=int(input(str(ip)+" Occupied!!plz enter an empty location : "))
+			if ip-1 not in choice:
+				c_ip=c_ip+1
+				continue
+			else:
+				user_ip=ip
+				break
+		choice.remove(user_ip-1)
+		board[user_ip-1]="X"
 		for i in [0,3,6]:
                         print("\t"+board[i]+" "+"|"+" "+board[i+1]+" "+"|"+" "+board[i+2])
 		if check(board):
@@ -74,10 +101,11 @@ def Tic_Tac_Toe():
 					print("Thank You")
 					return False
 		###################################################################################################
-		cmp_ip.append(random.choice(choice))
-                choice.remove(cmp_ip[len(cmp_ip)-1])
-                print("The computer picked :"+str(cmp_ip[len(cmp_ip)-1]))
-                board[cmp_ip[len(cmp_ip)-1]-1]="O"
+		#cmp_ip=int(random.choice(choice))
+		cmp_ip=int(Think(board,choice))
+                choice.remove(cmp_ip)
+                print("The computer picked :"+str(cmp_ip+1))
+                board[cmp_ip]="O"
                 #print("Board:\n")
                 for i in [0,3,6]:
                         print("\t"+board[i]+" "+"|"+" "+board[i+1]+" "+"|"+" "+board[i+2])
